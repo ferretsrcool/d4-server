6207141d79d6ce17a026bbf6a9939971549e0c0d
pringon <gojedan98@yahoo.com>
2019-02-20 21:41:39 +0000

    Change variable name from readingRouter to router in Reading controller

--- src/controllers/Reading.ts
+++ src/controllers/Reading.ts
@@ -3,26 +3,26 @@ import { Router, Request, Response } from 'express';
 import { Document } from 'mongoose';
 import Reading from '../models/Reading';
 
-const router = Router();
+const readingRouter = Router();
 
-router.get('/', (req: Request, res: Response) => {
+readingRouter.get('/', (req: Request, res: Response) => {
   Reading.find({})
   .then((readings: Document[]) => res.status(200).send(readings))
   .catch((err: Error) => res.status(500).send(err));
 });
 
-router.get('/:id', (req: Request, res: Response) => {
+readingRouter.get('/:id', (req: Request, res: Response) => {
   Reading.findById(req.params.id)
   .then((reading: Document | null) => {
     if (reading === null) {
-      res.status(401).send('Reading with given id could not be found');
+      res.status(401).send('Reading with given id could not be found.');
     }
     res.status(200).send(reading);
   })
   .catch((err: Error) => res.status(401).send(err));
 });
 
-router.post('/', (req: Request, res: Response) => {
+readingRouter.post('/', (req: Request, res: Response) => {
   Reading.create({
     user: req.body.user,
     samples: req.body.samples.split(','),
@@ -31,4 +31,4 @@ router.post('/', (req: Request, res: Response) => {
   .catch((err: Error) => res.status(400).send(err));
 });
 
-export default router;
+export default readingRouter;
